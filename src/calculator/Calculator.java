6ec2691494a1076efaa378b9cf595c43552e8f30
MontaMac <monta.pole@inbox.lv>
2018-08-23 20:31:43 +0300

    Calculate 2 operations

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,7 +8,52 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
+        double a = Double.parseDouble(expression[0]);
+        double b = Double.parseDouble(expression[2]);
+
+        double result;
+        switch (expression[1]) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                return "ERROR";
+        }
+        if (expression.length < 4) {
+            return String.valueOf(result);
+        } else {
+            double c = Double.parseDouble(expression[4]);
+            switch (expression[3]) {
+                case "+":
+                    result = result + c;
+                    break;
+                case "-":
+                    result = result - c;
+                    break;
+                case "*":
+                    result = result * c;
+                    break;
+                case "/":
+                    result = result / c;
+                    break;
+                default:
+                    return "ERROR";
+            }
+        }
+
+        return String.valueOf(result);
     }
 
 }
